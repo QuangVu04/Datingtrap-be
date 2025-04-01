66bbb078ee8bedd3fd5f1fc8b26e09ccff6ce5c1
QuangVu04 <quangvu1782004@gmail.com>
2025-04-01 09:29:03 +0700

    base

---
+++ src/main/java/com/example/datingtrap/DatingtrapApplication.java
@@ -0,0 +1,13 @@
+package com.example.datingtrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingtrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingtrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/datingtrap/DatingtrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datingtrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingtrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
